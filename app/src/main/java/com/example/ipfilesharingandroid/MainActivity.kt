b30aac0443a5b0be30397fffc196cf5b056003e8
andreicosmin02 <andrei.achim.6669@gmail.com>
2024-11-14 09:08:32 +0200

    Updated agp and compile sdk versions

--- app/src/main/java/com/example/ipfilesharingandroid/MainActivity.kt
+++ app/src/main/java/com/example/ipfilesharingandroid/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Helloo $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
